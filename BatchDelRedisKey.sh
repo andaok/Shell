d6d783c85f1aadae42474428888aea2bd1b3cafc
root <root@c02.cloudiya.com>
2013-05-13 18:00:01 +0800

    auto update data in cloudiya

--- BatchDelRedisKey.sh
+++ BatchDelRedisKey.sh
@@ -16,18 +16,28 @@ AllKeyList=$(redis-cli keys \* | awk '{print $1}' | xargs echo )
 
 AllKeyArray=( $AllKeyList )
 
-echo ${#AllKeyArray[*]}
+AllKeyNum=${#AllKeyArray[*]}
 
-for ((i=0;i<$NoDelKeyFlagNum;i++))
+
+for ((j=0;j<$AllKeyNum;j++))
 do
-   KeysList=$(redis-cli keys *${NoDelKeyFlag[i]} | awk '{print $1}') 
-   
-   for key in $KeysList
+   keyname=${AllKeyArray[j]}
+
+   KeyDel="True"
+
+   for ((i=0;i<$NoDelKeyFlagNum;i++))
    do
-      echo $key      
-   done  
+      KeysList=$(redis-cli keys *${NoDelKeyFlag[i]} | awk '{print $1}')  
+      for key in $KeysList
+      do
+         if [ "$keyname" == "$key" ]; then
+            KeyDel="False"
+         fi      
+      done
+   done
+
+   if [ "$KeyDel" == "True" ]; then
+      $(redis-cli del $keyname)
+   fi
+
 done
-
-
-
-
